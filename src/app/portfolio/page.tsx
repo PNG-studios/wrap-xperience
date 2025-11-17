b9c0367e445c9f244dc40654a8df0da8f102eb51
Broodje2 <106760184+Broodje2@users.noreply.github.com>
2025-11-17 09:39:07 +0100

    Added portfolioExample component
    Added a component for the portfolio page where you can choose photos to display

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,19 @@
 import Header from "@/components/header";
+import PortfolioExample from "@/components/portfolioExample";
 
 export default function Portfolio() {
   return (
     <>
       <main>
         <Header backgroundImage1="/images/hero.png" title="PORTFOLIO" />
+
+        <PortfolioExample
+          title="Test Title"
+          imageBefore={["/images/hero.png", "/images/hero.png"]}
+          altBefore={["image 1", "image 2"]}
+          imageAfter={["/images/hero.png", "/images/hero.png"]}
+          altAfter={["image 1", "image 2"]}
+        />
       </main>
     </>
   );
