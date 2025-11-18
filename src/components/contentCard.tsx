1da7bdd3eb61c9e060c6d5dc3fa9b05ea9b688fc
Broodje2 <106760184+Broodje2@users.noreply.github.com>
2025-11-18 14:30:25 +0100

    Fixed contentCard spacing

--- src/components/contentCard.tsx
+++ src/components/contentCard.tsx
@@ -1,5 +1,3 @@
-
-
 interface ContentCardProps {
   imageSection: React.ReactNode;
   title: string;
@@ -20,17 +18,29 @@ export default function ContentCard({
   backgroundColor,
 }: ContentCardProps) {
   return (
-    <section style={{backgroundColor}} className="flex my-15 justify-center gap-8 p-8 py-16">
-      <div className={`flex ${
-        "sm:" + (position === "right" ? "flex-row-reverse" : "flex-row")
-      } flex-col flex-col-reverse items-center justify-between gap-[7.5rem] w-full max-w-[1440px] mx-auto`}>
-        <div className="relative sm:w-1/2 w-full h-full">
-          {imageSection}
-        </div>       
-         <div className="sm:w-1/2 w-full space-y-4">
-          <h2 className="sm:text-3xl text-[5rem] text-primary font-bold text-dark">{title}</h2>
-          <p className="sm:text-xl text-[2.5rem] text-gray-600 leading-relaxed">{description}</p>
-          <a className="sm:text-xl text-[2.5rem] bg-primary hover:bg-primary/80 text-white px-6 py-3 p-5 rounded-xl transition-colors" href={buttonHref}>{buttonText}</a>
+    <section
+      style={{ backgroundColor }}
+      className="flex my-15 justify-center gap-8 p-8 py-16"
+    >
+      <div
+        className={`flex ${
+          "sm:" + (position === "right" ? "flex-row-reverse" : "flex-row")
+        } flex-col flex-col-reverse items-center justify-between gap-[7.5rem] w-full max-w-[1440px] mx-auto`}
+      >
+        <div className="relative sm:w-1/2 w-full h-full">{imageSection}</div>
+        <div className="sm:w-1/2 w-full sm:space-y-4">
+          <h2 className="sm:text-3xl text-[5rem] text-primary font-bold text-dark">
+            {title}
+          </h2>
+          <p className="sm:text-xl text-[2.5rem] text-gray-600 leading-relaxed">
+            {description}
+          </p>
+          <a
+            className="sm:text-xl text-[2.5rem] bg-primary hover:bg-primary/80 text-white px-6 py-3 p-5 rounded-xl transition-colors"
+            href={buttonHref}
+          >
+            {buttonText}
+          </a>
         </div>
       </div>
     </section>
