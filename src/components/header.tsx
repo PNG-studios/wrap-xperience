cc25dc8515eb568d813d53db27ca9370e4076aa9
Broodje2 <106760184+Broodje2@users.noreply.github.com>
2025-11-18 15:52:22 +0100

    Fixed portfolio page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ export default function Header({ backgroundImage1, title }: HeaderProps) {
         className="w-full sm:h-[50vh] h-[20vh] items-center mx-auto bg-cover flex flex-col justify-end"
       >
         <div className="max-w-[1440px] w-full ">
-          <h2 className="text-white sm:text-8xl text-[10rem] font-bold pb-8 sm:ml-0 ml-6">
+          <h2 className="text-white sm:text-8xl text-[12vw] font-bold pb-8 sm:ml-0 ml-6">
             {title}
           </h2>
         </div>
